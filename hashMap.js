d9ff28bba7fb5dcb804f562ac4716891e53a4f63
Shukri Rahmat <119567950+shukrirahmat@users.noreply.github.com>
2024-06-04 20:54:05 +0800

    did all the basics

--- hashMap.js
+++ hashMap.js
@@ -16,45 +16,129 @@ const hashMap = function hashMap() {
 
   const set = function set(key, value) {
     const index = hash(key) % buckets.length;
+    isIndexInRange(index);
 
-    if (index < 0 || index >= buckets.length) {
-      throw new Error("Trying to access index out of bound");
-    }
-
-    const linkedList = buckets[index];
-    const listIndex = linkedList.find(key);
+    const bucket = buckets[index];
+    const listIndex = bucket.find(key);
 
     if (listIndex !== null) {
-      linkedList.at(listIndex).value = value;
+      bucket.at(listIndex).value = value;
     } else {
-      linkedList.append(key);
-      linkedList.getTail().value = value;
+      bucket.append(key);
+      bucket.getTail().value = value;
     }
   };
 
   const get = function get(key) {
     const index = hash(key) % buckets.length;
+    isIndexInRange(index);
 
-    if (index < 0 || index >= buckets.length) {
-      throw new Error("Trying to access index out of bound");
-    }
-
-    const linkedList = buckets[index];
-    const listIndex = linkedList.find(key);
+    const bucket = buckets[index];
+    const listIndex = bucket.find(key);
 
     if (listIndex !== null) {
-      return linkedList.at(listIndex).value;
+      return bucket.at(listIndex).value;
     }
 
     return null;
   };
 
-  const view = () => buckets.map((list) => list.getHead());
+  const has = function has(key) {
+    const index = hash(key) % buckets.length;
+    isIndexInRange(index);
+
+    const bucket = buckets[index];
+    return bucket.contains(key);
+  };
+
+  const remove = function remove(key) {
+    const index = hash(key) % buckets.length;
+    isIndexInRange(index);
+
+    const bucket = buckets[index];
+    const listIndex = bucket.find(key);
+    if (listIndex !== null) {
+      bucket.removeAt(listIndex);
+      return true;
+    }
+
+    return false;
+  };
+
+  const length = function length() {
+    let total = 0;
+    buckets.forEach((bucket) => {
+      total += bucket.size();
+    });
+
+    return total;
+  };
+
+  const clear = function clear() {
+    buckets = [...new Array(capacity)].map(() => createLinkedList());
+  };
+
+  const keys = function keys() {
+    let list = [];
+    buckets.forEach((bucket) => {
+      let count = 0;
+      while (count < bucket.size()) {
+        list.push(bucket.at(count).key);
+        count++;
+      }
+    });
+
+    return list;
+  };
+
+  const values = function values() {
+    let list = [];
+    buckets.forEach((bucket) => {
+      let count = 0;
+      while (count < bucket.size()) {
+        list.push(bucket.at(count).value);
+        count++;
+      }
+    });
+
+    return list;
+  };
+
+  const entries = function entries() {
+    let list = [];
+    buckets.forEach((bucket) => {
+      let count = 0;
+      while (count < bucket.size()) {
+        list.push([bucket.at(count).key, bucket.at(count).value]);
+        count++;
+      }
+    });
+
+    return list;
+  };
+
+  const isIndexInRange = function isIndexInRange(index) {
+    if (index < 0 || index >= buckets.length) {
+      throw new Error("Trying to access index out of bound");
+    }
+  };
 
   return {
     hash,
     set,
     get,
-    view,
+    has,
+    remove,
+    length,
+    clear,
+    keys,
+    values,
+    entries,
   };
 };
+
+const map = hashMap();
+map.set("john", "doe");
+map.set("A", "almond");
+map.set("Q", "queen");
+console.log(map.entries());
